b4bf5765081ae8a8f4b3d431598db4d006ffdb5c
Nevena809 <nevena.milojevic@dev.telnet.rs>
2024-10-11 15:29:26 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-function App() {}
+import Header from "./componentes/Header";
+import Quiz from "./componentes/Quiz";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <main>
+        <Quiz></Quiz>
+      </main>
+    </>
+  );
+}
 
 export default App;
